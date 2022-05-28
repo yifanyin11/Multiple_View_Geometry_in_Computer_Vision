2e3070a011d1dd1154437132c2dad6941b79b8e5
yifanyin11 <96314762+yifanyin11@users.noreply.github.com>
2022-05-27 23:37:59 -0400

    Delete FundamentalMatrix.py

--- FundamentalMatrix.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import cv2 as cv
-def FundamentalMatrix( x, xp ):
-    x = np.asarray(x)
-    xp = np.asarray(xp)
-    F, mask = cv.findFundamentalMat(x, xp, cv.FM_8POINT)
-    return F
